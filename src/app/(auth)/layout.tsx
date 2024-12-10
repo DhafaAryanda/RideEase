b4b5e69bcb032bb3b2b56446f0d3c547bd058081
dhofaaryanda4@gmail.com <dhofaaryanda4@gmail.com>
2024-12-10 17:02:29 +0700

    feat(auth): add feature sign-in and sign-up

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import Navbar from "../components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,17 @@ export default function RootLayout({
       <body
         className={`${inter.className} text-white font-poppins bg-rideease-black`}
       >
-        {children}
+        <section
+          id="Signup"
+          className="bg-[url('/assets/images/background/seat.jpg')] bg-no-repeat bg-cover bg-left-top -z-10 min-h-screen"
+        >
+          <div className="Header-content bg-gradient-to-r from-[#080318] to-[rgba(8,3,24,0)] z=0 min-h-screen">
+            <Navbar />
+            <div className="flex flex-col justify-between min-h-[calc(100vh-78px)]">
+              {children}
+            </div>
+          </div>
+        </section>
       </body>
     </html>
   );
